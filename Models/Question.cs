3f09ebc9b8f8792d13233d9f4b9ffa8473d9dae8
Sean Keever <sean@ravenlogistics.com>
2020-07-17 12:41:52 -0400

    Inital commit to push all exisitng code for SurveyJsonBuilder to new repo

---
+++ Models/Question.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SurveyJsonBuilder.Models
+{
+    public class Question
+    {
+        public Question(string Description, bool AllowYesNo, bool AllowShortAnswer)
+        {
+            this.Description = Description;
+            this.AllowYesNo = AllowYesNo;
+            this.AllowShortAnswer = AllowShortAnswer;
+        }
+
+        public string Description { get; set; }
+        public bool AllowYesNo { get; set; }
+        public bool AllowShortAnswer { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using SurveyJsonBuilder.Models;
+using System;
+
+namespace SurveyJsonBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Question inputData = new Question("Test Description", true, true);
+            Console.WriteLine(JsonConvert.SerializeObject(inputData));
+
+            Console.WriteLine(inputData);
+        }
+    }
+}
